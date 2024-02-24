a7229295e7a6d67f7da24b80f70285d235165444
naveenney <159130020+naveenney@users.noreply.github.com>
2024-02-24 15:02:55 +0530

    Update AnnalynsInfiltration.java

--- AnnalynsInfiltration.java
+++ AnnalynsInfiltration.java
@@ -1,5 +1,32 @@
-package exercism;
 
 public class AnnalynsInfiltration {
+  	  public static boolean canFastAttack(boolean knightIsAwake) {
+	        
+	         if (knightIsAwake)
+	            return false;
+	         
+	           return true;
+	    }
+	    public static boolean canSpy(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake) {
+	      
+	    	if(archerIsAwake||knightIsAwake||prisonerIsAwake)
+	            return true;
+	        
+	        return false;
+	    }
+	    public static boolean canSignalPrisoner(boolean archerIsAwake, boolean prisonerIsAwake) {
+	        
+	    	if(!archerIsAwake&&prisonerIsAwake)
+	            return true;
+	    	
+	        return false;
+	    }
+	    public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake, boolean petDogIsPresent) {
+	        
+	    	if(petDogIsPresent&&!archerIsAwake||!petDogIsPresent&&!knightIsAwake&&!archerIsAwake&&prisonerIsAwake)
+	            return true;
+	
+	    	return false;      
+	           }
 
-}
+	}
